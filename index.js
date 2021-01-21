802582e405fdb56621a15e1b3b8fdd18e5d112a8
ibrahim <55027665+ibraheemybg@users.noreply.github.com>
2021-01-21 12:12:43 -0700

    Add techop official pages

---
+++ index.js
@@ -0,0 +1,3 @@
+document.querySelector("button").addEventListener("click",function(){
+  alert("Thank you for wanting to Donate to techop. Please fill the Donation form carefully to avoid errors.")
+});
